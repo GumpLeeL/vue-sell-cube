7f0e3ec8e6bed2162c51c9a44903751025f32136
lizhichuang <18303230863@163.com>
2020-06-04 15:40:52 +0800

    更新接口

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,44 @@
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const rating = appData.ratings
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      stylus: {
-        'resolve url': true,
-        import: [
-          './src/theme'
-        ]
-      }
+    css: {
+        loaderOptions: {
+            stylus: {
+                'resolve url': true,
+                import: ['./src/theme']
+            }
+        }
+    },
+    pluginOptions: {
+        'cube-ui': {
+            postCompile: true,
+            theme: true
+        }
+    },
+    // 数据接口
+    devServer: {
+        before(app) {
+            app.get('/api/seller', function(req, res) {
+                res.json({
+                    errno: 0,
+                    data: seller
+                })
+            })
+            app.get('/api/goods', function(req, res) {
+                res.json({
+                    errno: 0,
+                    data: goods
+                })
+            })
+            app.get('/api/rating', function(req, res) {
+                res.json({
+                    errno: 0,
+                    data: rating
+                })
+            })
+        }
     }
-  },
-  pluginOptions: {
-    'cube-ui': {
-      postCompile: true,
-      theme: true
-    }
-  }
 }
